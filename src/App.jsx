c9a5a0b96cfbe9264f556f456c18a8685de662ea
Brayan Duque <bradumar2016@gmail.com>
2025-03-25 15:01:13 -0500

    modificaciones cronometro 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
-import { React, useState, useRef } from "react";
-import "./App.css";
+import { useState, useRef } from "react";
 
 function App() {
-  // se crea el estado de iniciar el tiempo, el tiempo ahora y una referencia para guardar el tiempo todas con valor null
+  //Se crean las variables de estados las cuales se van a modificar
   const [startTime, setStartTime] = useState(null);
   const [now, setNow] = useState(null);
+  const [times, setTimes] = useState([]); 
   const intervalRef = useRef(null);
-  //Se crea una funcion para inicializar el tiempo y se guarda en el estado de tiempo ahora
+//se crean las funciones de los botones para su respectivo caso
   function handleStart() {
     setStartTime(Date.now());
     setNow(Date.now());
-
-    // se crea un intervalo para actualizar el tiempo ahora cada segundo y se guarda en la referencia intervalRef
+    //utilizamos la referencia de la variable para poder limpiar el intervalo y que no se acumulen
     clearInterval(intervalRef.current);
     intervalRef.current = setInterval(() => {
       setNow(Date.now());
     }, 10);
   }
-  //se crea una funcion para detener el tiempo y se limpia la referencia intervalRef
+
   function handleStop() {
     clearInterval(intervalRef.current);
+    if (startTime !== null && now !== null) {
+      // Calculamos el tiempo transcurrido y lo agregamos a la lista de tiempos
+      const elapsed = ((now - startTime) / 1000).toFixed(3);
+      // Agregar el tiempo a la lista de tiempos
+      setTimes((prevTimes) => [...prevTimes, elapsed]); 
+    }
   }
-  //se calcula el tiempo transcurrido en segundos y se muestra en pantalla
+
   let secondsPassed = 0;
-  if (startTime != null && now != null) {
+  // Calculamos los segundos transcurridos
+  if (startTime !== null && now !== null) {
     secondsPassed = (now - startTime) / 1000;
   }
+
   return (
-    <div className="container mx-auto text-center bg-cyan-200 p-4 my-10 rounded-lg shadow-lg">
-      {/* se muestra el tiempo transcurrido en pantalla y dos botones para iniciar y detener el tiempo */}
-      <h1 className="text-4xl font-bold ">Cronómetro React</h1>
-      <h2 className="mt-4 text-xl font-medium">{secondsPassed.toFixed(3)}</h2>
+    <div className="container mx-auto text-center bg-linear-65 from-purple-500 to-pink-500 p-4 my-10 rounded-lg shadow-lg">
+      <h1 className="text-4xl font-bold">Cronómetro React</h1>
+      <h2 className="mt-4 text-xl font-medium text-white">{secondsPassed.toFixed(3)}</h2>
       <div className="flex justify-center space-x-4 mt-4">
         <button
           className="bg-green-400 hover:bg-green-600 text-white font-semibold py-1 px-4 rounded"
@@ -42,9 +48,21 @@ function App() {
           className="bg-red-400 hover:bg-red-600 text-white font-semibold py-1 px-4 rounded"
           onClick={handleStop}
         >
-          Parar
+          Pausar
         </button>
       </div>
+
+      <h2 className="mt-4 text-xl font-medium ">Tiempos Registrados</h2>
+      <ul className="mt-2 text-lg">
+        {times.length === 0 ? (
+          <li className="text-gray-300">No hay tiempos registrados</li>
+        ) : null}
+        {times.map((time, index) => (
+          <li key={index} className="text-gray-200">{`Intento ${
+            index + 1
+          }: ${time}s`}</li>
+        ))}
+      </ul>
     </div>
   );
 }
